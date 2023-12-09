a8268cd1bab5178256f365888b64531c8d2d85b6
Alfred-Jijo <alfredjijo28@gmail.com>
2023-12-09 18:28:25 +0000

    qs

--- flashcards/flashcard.go
+++ flashcards/flashcard.go
@@ -6,8 +6,8 @@ import (
 )
 
 var flashcards = map[string]string{
-	"Quantity":            "In S.I. a quantity is represented by a number and a unit, (e.g. m = 3.0 kg).",
-	"Vector":              "A vector is a quantity that has magnitude and direction.",
+	"What is a Quantity":  "In S.I. a quantity is represented by a number and a unit, (e.g. m = 3.0 kg).",
+	"What is a Vector":    "A vector is a quantity that has magnitude and direction.",
 	"Newton's First Law":  "A body will remain in uniform motion in a straight line unless acted on by an external force.",
 	"Newton's Second Law": "The rate of change of a body’s momentum is directly proportional to the force active upon it.",
 }
