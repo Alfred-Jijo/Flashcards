3756b827b46da80151b99d121ac4d094cc5156c1
Alfred-Jijo <alfredjijo28@gmail.com>
2023-12-21 20:50:26 +0000

    going for web based

---
+++ src/web/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/epicnotgames/cli-flashcards/src/packages/utils"
+	"github.com/epicnotgames/cli-flashcards/src/web/handlers"
+)
+
+func WebInit() {
+	http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/flashcards", handlers.FlashcardsHandler)
+
+	utils.ErrHandle(http.ListenAndServe(":8080", nil))
+}
